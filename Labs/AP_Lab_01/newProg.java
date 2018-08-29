ced05cc6a21c1d0d4a7e6ab2ae8672767e88e468
Surya <suryamadhan@gmail.com>
2018-08-29 07:49:38 -0700

    Upload Lab 1

---
+++ Labs/AP_Lab_01/newProg.java
@@ -0,0 +1,30 @@
+public class newProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" _");
+		System.out.println("( \\");
+		System.out.println(" ) )");
+		System.out.println("( ( .-\"\"-.-\"\"-.  A.-A");
+		System.out.println("\\ \\/           \\/, , \\");
+		System.out.println(" \\ \\      =;      t  /=");
+		System.out.println("  \\   | \"\" .  \' ,---\' ");
+		System.out.println("  /  //     | ||");
+		System.out.println(" /_,))      |_,))");
+	}
+}
+
+/*  
+    _
+   ( \
+    ) )
+   ( (  .-""-.-""-.  A.-A
+    \ \/           \/, , \
+     \   \      =;    t  /=
+      \   | "" .  ' ,---'
+       / //    | ||
+      /_,))    |_,))
+*/
+
+
+
---
+++ Labs/AP_Lab_01/newProg2.java
@@ -0,0 +1,15 @@
+public class newProg2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  (c).-.(c)");
+		System.out.println("   / ._. \\");
+		System.out.println(" __\\( Y )/__");
+		System.out.println("(_.-/\'-\'\\-._)");
+		System.out.println("   || X ||");
+		System.out.println("   * \"-\" *");
+		System.out.println("  _.      ._");
+		System.out.println(" ( .-./\\.-. )");
+		System.out.println("   \"_\"  \"_\"");
+	}
+}
